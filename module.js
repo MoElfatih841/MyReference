6322b75dca7fd20a085388c536605f2692c434c8
mo.el.dev0l0 <mo.el.dev0l0.gmail.com>
2025-09-26 21:07:21 +0300

    creat footer for landing page project

---
+++ module.js
@@ -0,0 +1,12 @@
+// Ex:/
+// import elzero,{ a, arr} from "/Programing_References/javascript_reference.js";
+// console.log(a);
+// console.log(arr);
+// console.log(elzero());
+
+
+// Ex:/
+// import * as all from "/Programing_References/javascript_reference.js";
+// console.log(all.a);
+// console.log(all.arr);
+// console.log(all.elzero());
